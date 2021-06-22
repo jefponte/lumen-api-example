6085869e413596bfe4e325d903ab8d6c074925e0
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-06-22 18:52:38 -0300

    Change example

--- 01-simple-api/routes/web.php
+++ 01-simple-api/routes/web.php
@@ -16,6 +16,3 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->group(['prefix' => '/api'], function () use ($router) {
-    $router->get('/movie', 'MovieController@index');
-});
